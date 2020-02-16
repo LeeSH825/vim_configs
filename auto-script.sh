eaff036e5c64db49060b2a19c4c5b00dc7c171ef
LeeSH825 <rubilsh@naver.com>
2020-02-16 20:48:40 +0900

    fixed auto-script

--- auto-script.sh
+++ auto-script.sh
@@ -9,6 +9,6 @@ else
 	wget https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim -o ~/.vim/autoload/plug.vim
 	wget https://raw.githubusercontent.com/LeeSH825/vim-_configs/master/.vimrc -o ~/.vimrc
 fi
-vim -c "PluginInstall" -c "q" -c "q"
+vim -c "PlugInstall" -c "q" -c "q"
 echo color jellybeans >> ~/.vimrc
 rm -rf ../vim_configs
